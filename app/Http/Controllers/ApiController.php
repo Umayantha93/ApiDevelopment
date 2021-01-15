bb1e67dbb63613e814cb8f81f711cd9c46f4cee9
Umayantha93 <umayantha93@gmail.com>
2021-01-15 13:26:35 +0530

    Create a Student

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class ApiController extends Controller
 {
-    //
+    
+    public function getAllStudents(){
+
+
+    }
+    public function createStudent(Request $request){
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+
+        // return $response()->json(["Message" => "Stdent Record Created"], 201);
+        return response("Record Entered");
+    }
+
+    public function getStudent($id){
+
+    }
+    public function updateStudent(Request $request, $id){
+
+    }
+    public function deleteStudent($id){
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
-    Route::get('students', [ApiController::class, 'getAllStudents']);
-    Route::get('students/{id}', [ApiController::class, 'getStudent']);
-    Route::post('students', [ApiController::class, 'createStudent']);
-    Route::put('students/{id}', [ApiController::class, 'updateStudent']);
-    Route::delete('students/{id}', [ApiController::class, 'deleteStudent']);
+
 });
+
+Route::post('students', [ApiController::class, 'createStudent']);
+    // Route::get('students', [ApiController::class, 'getAllStudents']);
+    // Route::get('students/{id}', [ApiController::class, 'getStudent']);
+   
+    // Route::put('students/{id}', [ApiController::class, 'updateStudent']);
+    // Route::delete('students/{id}', [ApiController::class, 'deleteStudent']);
+
+
+
+
